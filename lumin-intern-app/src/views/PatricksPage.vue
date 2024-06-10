725b710a5cc70ecd83d2dc241bfaaa88e122b4de
Patrick Yuson <pyuson@Patricks-MBP.office.luminsmart.io>
2024-06-10 11:09:58 -0400

    Added a route for PatricksPage and got a text box to update a header upon change

---
+++ lumin-intern-app/src/views/PatricksPage.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+const name = ref('')
+</script>
+
+<template>
+    <h1>Welcome, {{ name }}</h1>
+    <div>
+        <input 
+            type="text" 
+            v-model="name"
+        >
+        </input>
+    </div>
+</template>
