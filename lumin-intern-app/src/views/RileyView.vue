993f0d2563437bfd57ac8a831e5b43c90426fd1c
rew2155 <rileywash02@gmail.com>
2024-06-10 12:39:15 -0400

    I have my own page

---
+++ lumin-intern-app/src/views/RileyView.vue
@@ -0,0 +1,10 @@
+<script setup>
+    import '../styles/RileysPage.css';
+</script>
+
+<template>
+        <div>
+            <h1>Riley's Page</h1>
+            <img src="https://media.licdn.com/dms/image/D4D03AQEAqtRGoqiM4g/profile-displayphoto-shrink_400_400/0/1711383312553?e=1723680000&v=beta&t=QCP0TjQ1hz_nd84Hypnn_-1vEWJPuEthCE2ZhqpDsQk"> 
+        </div>
+</template>
