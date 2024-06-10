dbc5e8784bbd038beeca1d4e1fe38808f344b4e7
rew2155 <rileywash02@gmail.com>
2024-06-10 14:58:24 -0400

    working on styling for my specific page

--- lumin-intern-app/src/views/RileyView.vue
+++ lumin-intern-app/src/views/RileyView.vue
@@ -2,9 +2,19 @@
     import '../styles/RileysPage.css';
 </script>
 
+<style>
+    .header{
+        text-align: center;
+    }
+
+    .image{
+        width:25%;
+    }
+</style>
+
 <template>
-        <div>
-            <h1>Riley's Page</h1>
-            <img src="https://media.licdn.com/dms/image/D4D03AQEAqtRGoqiM4g/profile-displayphoto-shrink_400_400/0/1711383312553?e=1723680000&v=beta&t=QCP0TjQ1hz_nd84Hypnn_-1vEWJPuEthCE2ZhqpDsQk"> 
+        <div className = "page-container">
+            <h1 className = "header">Riley's Page</h1>
+            <img className = "image" src="https://media.licdn.com/dms/image/D4D03AQEAqtRGoqiM4g/profile-displayphoto-shrink_400_400/0/1711383312553?e=1723680000&v=beta&t=QCP0TjQ1hz_nd84Hypnn_-1vEWJPuEthCE2ZhqpDsQk"> 
         </div>
 </template>
