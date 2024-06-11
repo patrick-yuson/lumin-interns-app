9a3f2af416978fe3cd25c19c28c8c6ad64b1e09e
Patrick Yuson <pyuson@Patricks-MBP.office.luminsmart.io>
2024-06-11 15:16:23 -0400

    set up a pinia store - going to learn how to use it next

---
+++ lumin-intern-app/src/components/Patrick/PatrickCounterSection.vue
@@ -0,0 +1,46 @@
+<script setup>
+</script>
+
+<template>
+    <div class="patrick-counter-div">
+        <div class="patrick-text-div">
+            <div class="patrick-counter-text">
+                This counter is being updated by Pinia:
+            </div>
+            <div class="patrick-count">
+                10
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+@import '../../styles/ColorPalette.css';
+.patrick-counter-div {
+    height: 100px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    width: 1220px;
+    margin: 75px 0px 0px 100px; /* top right bottom left */
+    border-radius: 30px;
+    padding: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: left;
+}
+
+.patrick-text-div {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+}
+
+.patrick-counter-text {
+    font-size: 40px;
+}
+
+.patrick-count {
+    font-size: 60px;
+    margin-left: 225px;
+    color: var(--yellow1)
+}
+</style>
---
+++ lumin-intern-app/src/stores/counter.js
@@ -0,0 +1,13 @@
+import { defineStore } from 'pinia'
+
+export const useDataStore = defineStore('counter', () => {
+    const count = ref(0)
+    const doubleCount = computed(() => count.value * 2)
+    function increment() {
+        count.value++
+    }
+
+    return { count, doubleCount, increment }
+})
+
+
